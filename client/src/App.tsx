4f1c348e683c6825b24eed17fe08c24b9f5e869f
Vineeth Viswanathan <vineeth0394@gmail.com>
2021-02-09 05:34:20 +0000

    Implement #1 filter input UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import AppHeader from './components/AppHeader';
+import GithubSearchFilter from './components/GithubSearchFilter';
 
 function App() {
   return (
     <div>
         <AppHeader />
+
+        <GithubSearchFilter />
     </div>
   );
 }
