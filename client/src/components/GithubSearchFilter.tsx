62cf1c3675ccdd1eb409efd71e510b1a0225a2b6
Vineeth Viswanathan <vineeth0394@gmail.com>
2021-02-09 08:14:37 +0000

    Implement useInput hook in githubFIlters

--- client/src/components/GithubSearchFilter.tsx
+++ client/src/components/GithubSearchFilter.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useInput } from '../hooks/useInput';
 
 function GithubSearchFilter() {
+
+  const { value:searchText, bind:bindSearchText, reset:resetSearchText } = useInput('');
+  const { value:filterType, bind:bindfilterType, reset:resetfilterType } = useInput('');
+
+
   return (
     <div className="filter-wrapper">
         <div className="search-text-input">
-            <input type="search" name="search" id="search" placeholder="Start typing to search .."/>
+            <input 
+                type="search" 
+                name="search"
+                id="search"
+                placeholder="Start typing to search .." 
+                {...bindSearchText}
+            />
         </div>
         <div className="search-type-input select-input-wrapper">
-            <select name="type" id="type">
+            <select name="type" id="type" {...bindfilterType}>
                 <option value="users">Users</option>
                 <option value="repositories">Repositories</option>
             </select>
