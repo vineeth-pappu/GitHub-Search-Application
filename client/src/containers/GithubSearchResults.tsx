03c842a7874465c2bfaf9ddd7d187759e31fc89b
Vineeth Viswanathan <vineeth0394@gmail.com>
2021-02-10 08:49:35 +0000

    Implement repo cards UI
    ref: #3

--- client/src/containers/GithubSearchResults.tsx
+++ client/src/containers/GithubSearchResults.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useEffect } from 'react';
+import React, { useMemo, useEffect, useState } from 'react';
 import AppHeader from '../components/AppHeader';
 import GithubSearchFilter from '../containers/GithubSearchFilter';
 import { useSelector, useDispatch } from 'react-redux';
@@ -14,19 +14,61 @@ function GithubSearchResults() {
   const searchTypeFilter = useSelector(searchTypeFilterSelector)
   const loading = useSelector(loadingSelector)
 
+  const [repoResult, setRepoResult] = useState({} as any)
+
   useEffect(() => {
     if (searchTextFilter && searchTextFilter.length >= 3) {
         dispatch(Loading(true))
+        fetchRepos()
     } else {
         dispatch(Loading(false))
     }
-  }, [searchTextFilter])    
+  }, [searchTextFilter])
+
+  const fetchRepos = async () => {
+    const query = "q=tetris+language:assembly&sort=stars&order=desc"
+    const response = await fetch("https://api.github.com/search/repositories?"+query, {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            'Accept': 'application/vnd.github.v3+json'
+        },
+    });
+    const result = await response.json()
+    setRepoResult(result);
+    console.log('fetchRepos', result);
+    dispatch(Loading(false))
+    
+    // return response.json(); // parses JSON response into native JavaScript objects
+  }
 
   if (loading) return <SearchResultsLoader />
 
   return (
     <div className="github-search-results-wrapper">
-        results
+        {
+            repoResult && repoResult.items && repoResult.items.map((d: any, i: any) => {
+                return (
+                    <div className="repo-card">
+                        <div className="repo-card-header">
+                            <div className="repo-name">Reponame</div>
+                            <div className="repo-desc">
+                                This is sample description. This is sample description
+                            </div>
+                        </div>
+                        <div className="repo-card-footer">
+                            <div className="footer-item">
+                                <div className="footer-item-icon">icon</div>
+                                <div className="footer-item-text">200</div>
+                            </div>
+                            <div className="footer-item">
+                                <div className="footer-item-icon">icon</div>
+                                <div className="footer-item-text">200</div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })
+        }
     </div>
   );
 }
