e2292d0498243cef0f8e18619186aa4c0cf77477
vineeth-pappu <vineeth0394@gmail.com>
2021-02-12 19:44:05 +0530

    Implement github service skeleton

--- server/app/github-searcher/http/controllers/githubSearchController.ts
+++ server/app/github-searcher/http/controllers/githubSearchController.ts
@@ -1,11 +1,22 @@
 import { Request, Response, Router } from 'express';
 import BaseController from '../../../common/baseController';
 import HttpResponse from '../../../common/facades/httpResponse';
+import httpClient from '../../../common/utils/httpClient'
+import githubSearchService from '../../services/githubSearch.service';
 
 class GithubSearchController extends BaseController {
 
     async searchUsers(req: Request, res: Response) {
-        res.status(HttpResponse.HTTP_OK).send(`List of users`);
+        try {
+            const query = "qs=tom"
+            const { data } = await githubSearchService.users(query)
+
+            res.status(HttpResponse.HTTP_OK).send(data);
+        } catch (error) {
+            // console.log('error---------', error);
+            // throw new Error("users failed")
+            res.status(HttpResponse.HTTP_UNPROCESSABLE_ENTITY).send(error);
+        }
 
     }
 
---
+++ server/app/github-searcher/services/githubSearch.service.ts
@@ -0,0 +1,18 @@
+import httpClient from '../../common/utils/httpClient'
+
+class GithubSearchService {
+    constructor() {
+
+    }
+
+    users(query: string) {
+        return httpClient.get("https://api.github.com/search/users?" + query, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/vnd.github.v3+json'
+            },
+        })
+    }
+}
+
+export default new GithubSearchService();
