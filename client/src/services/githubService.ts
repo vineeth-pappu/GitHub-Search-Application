9d16716ac859f68c1efa04be2064e466e04fdb90
vineeth-pappu <vineeth0394@gmail.com>
2021-02-14 18:13:13 +0530

    Add envoronment variables for client

--- client/src/services/githubService.ts
+++ client/src/services/githubService.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+const API_URL = process.env.REACT_APP_API_URL
 interface ISearchQuery {
     q: string,
     sort?: string,
@@ -7,13 +8,13 @@ interface ISearchQuery {
 }
 
 export const getUsers = (payload: ISearchQuery) => {
-    return axios.get("http://localhost:3000/GithubSearcher/users", {
+    return axios.get(`${API_URL}/GithubSearcher/users`, {
         params: payload
     })
 }
 
 export const getRepositories = (payload: ISearchQuery) => {
-    return axios.get("http://localhost:3000/GithubSearcher/repositories", {
+    return axios.get(`${API_URL}/GithubSearcher/repositories`, {
         params: payload
     })
 }
